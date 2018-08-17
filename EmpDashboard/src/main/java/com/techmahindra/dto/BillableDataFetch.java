92050ca58b180eba69232492de7518583cede9a5
SB00503408 <SB00503408@INPUHJPC13558.ATT.TechMahindra.com>
2018-08-17 17:32:28 +0530

    commit

---
+++ EmpDashboard/src/main/java/com/techmahindra/dto/BillableDataFetch.java
@@ -0,0 +1,11 @@
+package com.techmahindra.dto;
+
+import com.techmahindra.model.BillableModel;
+
+public class BillableDataFetch {
+
+	public static BillableModel setBillableModel(BillableModel billableModel) {
+		
+		return billableModel;
+	}
+}
